3fcc462ba8767e6586920abf8ff8c095190fc3d7
Emrullah Cevizbaş <cevizbas01@gmail.com>
2024-08-01 00:08:51 +0300

    Proje dosyası ekle.

---
+++ Week4Proje1/Class.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Proje1
+{
+     public class Class
+    {
+        private string name;
+        private string surname;
+        private String birthday;// bir işlem yapmayacaım için string türünde tanımlayacam
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+        public string Birthday
+        {
+            get
+            {
+                return birthday;
+            }
+            set
+            {
+                birthday = value;
+            }
+        }
+        
+        public void Person ()  // isimleri yazdırabileceğim bir metot tanımlıyorum.
+        {
+            Console.WriteLine($"İsmim {name} soyadım {surname} {birthday} tarihinde doğdum");
+        }
+    }
+
+}
---
+++ Week4Proje1/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using Week4Proje1;
+
+
+
+Class student1 = new Class();
+
+student1.Name = "Emrullah";
+student1.Surname = "CEVİZBAŞ";
+student1.Birthday = "22 / 10 / 2001";
+
+student1.Person();
+
+Class student2 = new Class();
+
+student2.Name = "Sezen";
+student2.Surname = "AKSU";
+student2.Birthday = "15 / 07 / 1954";
+
+student2.Person();
+
+Class student3 = new Class();
+
+student3.Name = "Şebnem";
+student3.Surname = "FERAH";
+student3.Birthday = "12 / 04 / 1972";
+
+student3 .Person();
+
+Class teacher1 = new Class();
+
+teacher1.Name = "Cem";
+teacher1.Surname = "KARACA";
+teacher1.Birthday = "05 / 04 / 1945";
+
+teacher1 .Person();
+
+Class teacher2 = new Class();
+
+teacher2.Name = "Barış";
+teacher2.Surname = "MANÇO";
+teacher2.Birthday = "02 / 01 / 1943";
+ 
